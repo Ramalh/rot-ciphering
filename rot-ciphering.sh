9d7f6777604092546e1d66a9c5e6aff2b559bf38
ramalh <hramal45@gmail.com>
2022-07-18 11:36:49 +0300

    adding loop

--- rot-ciphering.sh
+++ rot-ciphering.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-read -s -p 'Type your pass: ' pass
-echo
-read -p 'Type rotation position: ' rot
-
-if [[ $rot == "" ]]
-then
-		echo add second argument
-		exit 0
-fi
-
-if (($rot > 26))
-then
-		echo second argument can not be greater than 25
-		exit 0
-elif (($rot < 1))
-then
-		echo first argument can not be lower than 1
-		exit 0
-fi
-
-arr=()
-for i in {{a..z},{A..Z}}
+echo 'Type ^C (ctrl + c)'
+while [ TRUE ]
 do
-		arr+=($i)
+	read -s -p 'Type your pass: ' pass
+	echo
+	read -p 'Type rotation position: ' rot
+
+	if [[ $rot == "" ]]
+	then
+			echo add second argument
+			exit 0
+	fi
+
+	if (($rot > 26))
+	then
+			echo second argument can not be greater than 25
+			exit 0
+	elif (($rot < 1))
+	then
+			echo first argument can not be lower than 1
+			exit 0
+	fi
+	
+	arr=()
+	for i in {{a..z},{A..Z}}
+	do
+			arr+=($i)
+	done
+
+	echo $pass | tr a-zA-Z ${arr[$rot]}-za-${arr[$rot-1]}${arr[$rot+26]}-ZA-${arr[$rot+25]}
 done
-
-echo $pass | tr a-zA-Z ${arr[$rot]}-za-${arr[$rot-1]}${arr[$rot+26]}-ZA-${arr[$rot+25]}
-
